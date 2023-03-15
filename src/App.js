c211575b90c9130c7f8b5544e77fca6e513b9294
Pitou911 <songpitou723@gmail.com>
2023-03-15 14:29:23 -0500

    rebuild the library

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Featured from "./components/Featured";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className='App'>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+    </div>
+  );
+}
+
+export default App;
